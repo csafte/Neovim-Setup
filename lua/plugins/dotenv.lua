80d47a65058f7477957f638f9dfee512ccfd791c
safte <gim950733@gmail.com>
2026-01-08 19:09:47 +0900

    :pencil2: dotenv.lua edit

--- lua/plugins/dotenv.lua
+++ lua/plugins/dotenv.lua
@@ -1,12 +1,12 @@
 -- lua/plugins/dotenv.lua
 return {
-  "ellisonleao/dotenv.nvim",
-  priority = 1000,
-  config = function()
-    require("dotenv").setup({
-      enable_on_load = true,
-      verbose = false,
-      -- 핵심 수정: config 폴더 안의 lua 폴더 안에 있는 .env를 가리킵니다.
-          })
-  end,
+	"ellisonleao/dotenv.nvim",
+	priority = 1000,
+	config = function()
+		require("dotenv").setup({
+			enable_on_load = true,
+			verbose = false,
+	    path = vim.fn.stdpath("config") .. "/.env"
+    })
+	end,
 }
